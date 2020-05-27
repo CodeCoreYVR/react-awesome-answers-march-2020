3d9f9050e43b4afc272a829d6fff697c9a023e51
Brandon Y Lam <me@brandonlam.ca>
2020-05-27 10:25:59 -0700

    add demo to delete question from question Index

--- src/QuestionIndexPage/index.js
+++ src/QuestionIndexPage/index.js
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import questionData from '../questionData';
 import QuestionDetails from '../QuestionDetails';
 
-export default function QuestionIndexPage() {
-  return(
-    <main>
-      <h1>Question Index page</h1>
-      <ul>
-        { questionData.map( question => {
-          return(
-            <QuestionDetails
-              key={question.id}
-              title={question.title}
-              body={question.body}
-              view_count={question.view_count}
-              created_at={question.created_at}
-            />
-          )
-        })}
-      </ul>
-    </main>
-  )
+class QuestionIndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: questionData,
+      helloWorld: 'helloWorld'
+    }
+  }
+  
+  // all functions in ReactJS should be pure functions
+  // pure functions are functions that do not change anything outside of itself
+  deleteQuestion() {
+    console.log('question delete fired')
+    // everytime you call this.setState React knows to re-render this component
+    this.setState((state) => {
+      // const newState = state.questions.pop()
+      const questionsClone = [...state.question]
+      // console.log(state);
+      // const newQuestions = [...state.question] // we use the ... (spread operator) to copy the array of state.question
+      questionsClone.pop()
+      return {
+        question: questionsClone
+      }
+    })
+  }
+
+  // the render method relies on this.state to create views
+  render() {
+    console.log(this.state)
+    const questions = this.state.question.map( question => {
+      return(
+        <QuestionDetails
+          key={question.id}
+          title={question.title}
+          body={question.body}
+          view_count={question.view_count}
+          created_at={question.created_at}
+        />
+      )
+    })
+
+    return (
+      <main>
+        <h1>Question Index page</h1>
+        <ul>
+          { questions }
+        </ul>
+        <button onClick={() => { this.deleteQuestion() }}
+        >
+          Delete
+        </button>
+      </main>
+    )
+  }
 }
+
+export default QuestionIndexPage
--- src/QuestionShowPage/index.js
+++ src/QuestionShowPage/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionDetails from '../QuestionDetails';
 import AnswersList from '../AnswersList';
-import data from '../oneQuestionData';
+import oneQuestionData from '../oneQuestionData';
 
-export default function QuestionShowPage() {
-  return(
-    <div id='questionShowPage'>
-      <QuestionDetails
-        title={data.title}
-        body={data.body}
-        author={data.author}
-        view_count={data.view_count}
-        created_at={new Date(data.created_at)}
-      />
-      <AnswersList answers={data.answers}/>
-    </div>
-  )
+// If your component needs state or events... use a class based component
+class QuestionShowPage extends Component {
+  constructor(props) {
+    super(props); // calling the constructor() method of the React.Component class.
+    // call super(props) in every class component you write.
+    this.state = {
+      question: oneQuestionData
+    }
+  }
+
+  render () {
+    return (
+      <div id='questionShowPage'>
+        <QuestionDetails
+          title={this.state.question.title}
+          body={this.state.question.body}
+          author={this.state.question.author}
+          view_count={this.state.question.view_count}
+          created_at={new Date(this.state.question.created_at)}
+        />
+        <AnswersList answers={this.state.question.answers}/>
+      </div>
+    )
+  }
 }
+
+export default QuestionShowPage

3c81b781d537d45af6691a41352bc0b01344a828
Brandon Y Lam <me@brandonlam.ca>
2020-06-01 09:30:54 -0700

    implement sign in

--- src/SignInPage/index.js
+++ src/SignInPage/index.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Session } from '../requests';
 
 class SignInPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: []
+    }
+    this.createSession = this.createSession.bind(this);
+  }
+
+  createSession(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    // submit a request to sessions#create
+    Session.create({
+      email: formData.get('email'),
+      password: formData.get('password')
+    }).then(data => {
+      if (data.status === 404) {
+        this.setState({
+          errors: [{ message: 'Wrong credentails'}]
+        })
+      } else {
+        this.props.history.push('/')
+      }
+    })
+  }
+
   render() {
+    const { errors } = this.state;
     return(
       <main>
         <h1>Sign In</h1>
-        <form>
+        {
+          errors.length > 0 ? (
+            <div>
+              { errors.map(e => e.message).join(', ')}
+            </div>
+          ) : null
+        }
+        <form onSubmit={this.createSession}>
           <div>
             <label htmlFor='email'>Email</label>
             <input type='email' name='email' id='email' />

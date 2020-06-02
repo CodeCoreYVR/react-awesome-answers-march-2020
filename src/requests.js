41a8db510c3f351b3344e5ba2b94834106648da6
Hano <hinsul3113@gmail.com>
2020-06-02 09:33:10 -0700

    Add update and destroy requests to Question model

--- src/requests.js
+++ src/requests.js
@@ -1,72 +1,91 @@
-const BASE_URL = `http://localhost:3000/api/v1`
+const BASE_URL = `http://localhost:3000/api/v1`;
 export const Question = {
-    index() {
-        return fetch(`${BASE_URL}/questions`, {
-            credentials: 'include'
-        }).then(res => {
-            console.log(res);
-            return res.json();
-        })
-    },
-    create(params) {
-        return fetch(`${BASE_URL}/questions`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(params)
-        }).then(res => res.json());
-    },
-    get(id) {
-        return fetch(`${BASE_URL}/questions/${id}`, {
-            credentials: 'include'
-        }).then(res => res.json());
-    }
-}
+  index() {
+    return fetch(`${BASE_URL}/questions`, {
+      credentials: "include",
+    }).then((res) => {
+      console.log(res);
+      return res.json();
+    });
+  },
+  create(params) {
+    return fetch(`${BASE_URL}/questions`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }).then((res) => res.json());
+  },
+  get(id) {
+    return fetch(`${BASE_URL}/questions/${id}`, {
+      credentials: "include",
+    }).then((res) => res.json());
+  },
+  // Edit a question
+  update(id, params) {
+    return fetch(`${BASE_URL}/questions/${id}`, {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }).then((res) => res.json());
+  },
+
+  // Delete a Question
+  destroy(id) {
+    return fetch(`${BASE_URL}/questions/${id}`, {
+      method: "DELETE",
+      credentials: "include",
+    }).then((res) => res.json());
+  },
+};
 
 export const Session = {
-    create(params) {
-        // params should be an object with { email: 'some@gmail.com', password: 'supersecret' }
-        return fetch(`${BASE_URL}/session`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(params)
-        }).then((res) => {
-            return res.json();
-        })
-    }
-    // to use create
-    // Session.create({email: '', password: ''}).then(data => { console.log(data)})
-}
+  create(params) {
+    // params should be an object with { email: 'some@gmail.com', password: 'supersecret' }
+    return fetch(`${BASE_URL}/session`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }).then((res) => {
+      return res.json();
+    });
+  },
+  // to use create
+  // Session.create({email: '', password: ''}).then(data => { console.log(data)})
+};
 
 export const User = {
-    current() {
-        return fetch(`${BASE_URL}/users/current`, {
-            method: 'GET',
-            credentials: 'include'
-        }).then(res => res.json());
-    },
+  current() {
+    return fetch(`${BASE_URL}/users/current`, {
+      method: "GET",
+      credentials: "include",
+    }).then((res) => res.json());
+  },
 
-    create(params) {
-        return fetch(`${BASE_URL}/users`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(params)
-        }).then((res) => res.json())
-    }
-}
+  create(params) {
+    return fetch(`${BASE_URL}/users`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }).then((res) => res.json());
+  },
+};
 
 // shorthand for doing: { Question: Question, Session: Session }
 // this works because there are variables called Question & Session
 export default {
   Question,
   Session,
-  User
-}
+  User,
+};

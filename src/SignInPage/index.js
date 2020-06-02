cb111af9ce3ffb3a916de6b04d71d7b53784236b
Hano <hinsul3113@gmail.com>
2020-06-02 15:03:39 -0700

    Style SignInPage component

--- src/SignInPage/index.js
+++ src/SignInPage/index.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { Session } from '../requests';
+import React, { Component } from "react";
+import { Session } from "../requests";
 
 class SignInPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      errors: []
-    }
+      errors: [],
+    };
     this.createSession = this.createSession.bind(this);
   }
 
@@ -17,46 +17,48 @@ class SignInPage extends Component {
 
     // submit a request to sessions#create
     Session.create({
-      email: formData.get('email'),
-      password: formData.get('password')
-    }).then(data => {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    }).then((data) => {
       if (data.status === 404) {
         this.setState({
-          errors: [{ message: 'Wrong credentails'}]
-        })
+          errors: [{ message: "Wrong credentails" }],
+        });
       } else {
-        this.props.history.push('/')
+        this.props.history.push("/");
       }
-    })
+    });
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.props);
     const { errors } = this.state;
-    return(
-      <main>
-        <h1>Sign In</h1>
-        {
-          errors.length > 0 ? (
-            <div>
-              { errors.map(e => e.message).join(', ')}
-            </div>
-          ) : null
-        }
-        <form onSubmit={this.createSession}>
-          <div>
-            <label htmlFor='email'>Email</label>
-            <input type='email' name='email' id='email' />
+    return (
+      <main className="ui clearing segment Page">
+        <h1 className="ui center aligned header">Sign In</h1>
+        {errors.length > 0 ? (
+          <div className="ui negative message">
+            {errors.map((e) => e.message).join(", ")}
           </div>
-          <div>
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' id='password' />
+        ) : null}
+        <form className="ui large form" onSubmit={this.createSession}>
+          <div className="field">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" />
           </div>
-          <input type='submit' value='Sign In'/>
+          <div className="field">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+          <input
+            className="ui right floated large orange button"
+            type="submit"
+            value="Sign In"
+          />
         </form>
       </main>
-    )
+    );
   }
 }
 
-export default SignInPage
+export default SignInPage;
